c00f4c551dc6f7adc67df028f63c74b710a870bf
WeiYuanStudio <WeiYuanStudio@gmail.com>
2019-11-22 08:37:35 +0800

    :bug: 删除无用代码，修复index相应码500问题

--- src/main/java/club/piclight/LibraryManLite/View/Index.java
+++ src/main/java/club/piclight/LibraryManLite/View/Index.java
@@ -19,6 +19,5 @@ public class Index extends HttpServlet {
             RequestDispatcher dispatcher = req.getRequestDispatcher("/WEB-INF/books.jsp");
             dispatcher.forward(req, resp);
         }
-        super.doGet(req, resp);
     }
 }
