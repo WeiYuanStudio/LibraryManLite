7c4ae36b4b64c541c69cb8e3847e82a057b1c118
WeiYuanStudio <WeiYuanStudio@gmail.com>
2019-11-22 10:04:05 +0800

    :sparkles: 完善用户登录

--- src/main/java/club/piclight/LibraryManLite/DAO/UserDAO.java
+++ src/main/java/club/piclight/LibraryManLite/DAO/UserDAO.java
@@ -1,14 +1,26 @@
 package club.piclight.LibraryManLite.DAO;
 
 import club.piclight.LibraryManLite.Model.User;
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.List;
 
 public class UserDAO {
-    public static User login(String username, String password) {
-        User tmpUser = new User(1,
-                username,
-                'M',
-                "gmail",
-                "123");
-        return tmpUser;
+    public static User login(String username, String password) throws IOException {
+        String resource = "./mybatis-config.xml";
+        InputStream inputStream = Resources.getResourceAsStream(resource);
+        SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
+        SqlSession session = sqlSessionFactory.openSession();
+        List<User> userList = session.selectList("getUserByName", username);
+        User user = userList.get(0); //获取查询到的第一个用户
+        if (password.equals(userList.get(0).getPassword())) {
+            return userList.get(0);
+        }
+        return null;
     }
 }
