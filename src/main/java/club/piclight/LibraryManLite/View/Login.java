a4f32582d64a9f59719195565cc823e356de5cd1
WeiYuanStudio <WeiYuanStudio@gmail.com>
2019-11-26 21:45:03 +0800

    :recycle: 重构登录逻辑

--- src/main/java/club/piclight/LibraryManLite/View/Login.java
+++ src/main/java/club/piclight/LibraryManLite/View/Login.java
@@ -21,19 +21,22 @@ public class Login extends HttpServlet {
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        String username = req.getParameter("username"); //取请求参数用户名
-        String password = req.getParameter("password"); //取请求参数密码
-        if (!username.equals("") || !password.equals("")) {
+        try {
+            String username = req.getParameter("username"); //取请求参数用户名
+            String password = req.getParameter("password"); //取请求参数密码
+            if (username.equals("") || password.equals(""))
+                throw new IllegalArgumentException("Post Parameter Illegal");
             User loginUser = UserDAO.login(username, password); //数据库请求登录
-            if (loginUser != null) { //如果登录成功
+            if (loginUser == null)
+                throw new RuntimeException("Login Info Illegal");
                 req.getSession().setAttribute("user", loginUser); //设置Session
                 //转发到成功登录页，该页面js控制登录后跳转
                 RequestDispatcher dispatcher = req.getRequestDispatcher("/WEB-INF/loginsuccess.html");
                 dispatcher.forward(req, resp);
-            }
+        } catch (Exception e) {
+            //登录失败转发到失败页
+            RequestDispatcher dispatcher = req.getRequestDispatcher("/WEB-INF/loginfailed.html");
+            dispatcher.forward(req, resp);
         }
-        //登录失败转发到失败页
-        RequestDispatcher dispatcher = req.getRequestDispatcher("/WEB-INF/loginfailed.html");
-        dispatcher.forward(req, resp);
     }
 }
