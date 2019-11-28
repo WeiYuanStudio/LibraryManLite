a35ea46a4e7e50389c0fde4c50d22a77bbe0020d
WeiYuanStudio <WeiYuanStudio@gmail.com>
2019-11-28 13:54:40 +0800

    :truck: 用户详情页移至WEB-INF

---
+++ src/main/java/club/piclight/LibraryManLite/View/UserInfo.java
@@ -0,0 +1,18 @@
+package club.piclight.LibraryManLite.View;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(urlPatterns = "/userinfo")
+public class UserInfo extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        RequestDispatcher dispatcher = req.getRequestDispatcher("/WEB-INF/userinfo.jsp");
+        dispatcher.forward(req, resp);
+    }
+}
